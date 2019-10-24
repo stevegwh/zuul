1267f0c8e4482ab6caddcf92f1f239d116e3fd0b
Steve <sgwheeler@protonmail.com>
2019-10-24 12:18:25 +0100

    OutputLayers replace Events

--- src/commandhandler/CommandOutputLayer.java
+++ src/commandhandler/CommandOutputLayer.java
@@ -1,5 +1,7 @@
 package commandhandler;
 
+// TODO: Change commandHandler.commandOutputLayers to commandHandler + ioMode + OutputLayers
+// TODO: Move to Output Layer package to see if visibility is restricted or not.
 public interface CommandOutputLayer {
 	void init(String[] args);
 	void onSuccess();
