14e524dd570476e1b5ae01d81a91f53ad5310a65
Steve <sgwheeler@protonmail.com>
2019-10-24 16:59:11 +0100

    Pre-RoomController de-static

--- src/zuul/Player.java
+++ src/zuul/Player.java
@@ -3,6 +3,11 @@ package zuul;
 public class Player {
 	private InventoryController inventory = new InventoryController();
 	private String currentLocation;
+	private RoomController roomController = new RoomController();
+
+	public RoomController getRoomController() {
+		return roomController;
+	}
 	public InventoryController getInvController() {
 		return inventory;
 	}
